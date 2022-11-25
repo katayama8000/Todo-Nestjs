98e35c27d138225aa0458087a53d611713f823ba
katayama <tattu.0310@gmail.com>
2022-11-25 20:58:55 +0900

    feat: db

---
+++ src/_tutorial/typeorm/employee.entity.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { ContactInfo } from './contact-info.entity';
+import { Meeting } from './meeting.entiry';
+import { Task } from './task.entity';
+
+@Entity()
+export class Employee {
+  [x: string]: any;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @ManyToMany(() => Employee, (employee) => employee.directReports, {
+    onDelete: 'SET NULL',
+  })
+  manager: Employee;
+
+  @OneToMany(() => Employee, (employee) => employee.manager)
+  directReports: Employee[];
+
+  @OneToOne(() => ContactInfo, (contactInfo) => contactInfo.employee)
+  contactInfo: ContactInfo;
+
+  @OneToMany(() => Task, (task) => task.employee)
+  tasks: Task[];
+
+  @ManyToMany(() => Meeting, (meeting) => meeting.atenndees)
+  @JoinTable()
+  meetings: Meeting[];
+}
---
+++ src/_tutorial/typeorm/index.ts
@@ -0,0 +1,6 @@
+import { ContactInfo } from './contact-info.entity';
+import { Employee } from './employee.entity';
+import { Meeting } from './meeting.entiry';
+import { Task } from './task.entity';
+
+export default [ContactInfo, Employee, Meeting, Task];
